eb1c9b62abddcb1c4826d5c3e9e1c61f79045563
Ferran Bertomeu Castells <fonserbc@gmail.com>
2013-03-22 16:01:06 +0100

    Added spherize fragment shader

--- src/com/fonserbc/bouncegl/GameGLRenderer.java
+++ src/com/fonserbc/bouncegl/GameGLRenderer.java
@@ -26,6 +26,7 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
     private final float[] mNormalMatrix = new float[16];
     
     private Plane[] planes;
+    private float[][] colors;
     
     private int basicShader;
     
@@ -37,6 +38,8 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
     private float mVYAngle = 0;
     
     private float[] mCurrRotation = new float[16];
+    private float[] mLightPos = new float[2];
+    private int mLightState = 0;
     
     private enum RotationMode {
     	Normal,
@@ -57,8 +60,8 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
 		Matrix.setLookAtM(mVMatrix, 0,  0,0,1.5f,  0,0,0,  0,1,0);
 		Matrix.setIdentityM(mCurrRotation, 0);
 		
-		String vertexCode = Utilities.stringFromResource(mContext, R.raw.testvert3);
-		String fragmentCode = Utilities.stringFromResource(mContext, R.raw.testfrag3);
+		String vertexCode = Utilities.stringFromResource(mContext, R.raw.vertexnormalzcoord);
+		String fragmentCode = Utilities.stringFromResource(mContext, R.raw.spherefrag);
 		
 		int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexCode);
 		int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentCode);
@@ -75,13 +78,13 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
         GameGLRenderer.checkGlError("glGetUniformLocation");
 		GLES20.glUniform1fv(mSpeedPointer, 1, new float[] {3}, 0);
 		
-		int mTilingPointer = GLES20.glGetUniformLocation(basicShader, "tiling");
+		int mLightHeightPointer = GLES20.glGetUniformLocation(basicShader, "lightHeight");
         GameGLRenderer.checkGlError("glGetUniformLocation");
-		GLES20.glUniform2fv(mTilingPointer, 1, new float[] {0, 0}, 0);
+		GLES20.glUniform1fv(mLightHeightPointer, 1, new float[] {3f}, 0);
 		
-		int mStrokePointer = GLES20.glGetUniformLocation(basicShader, "lightDivs");
+		int mLightDivs = GLES20.glGetUniformLocation(basicShader, "lightDivs");
         GameGLRenderer.checkGlError("glGetUniformLocation");
-		GLES20.glUniform1fv(mStrokePointer, 1, new float[] {5f}, 0);
+		GLES20.glUniform1fv(mLightDivs, 1, new float[] {10f}, 0);
 		
 		GLES20.glEnable(GLES20.GL_DEPTH_TEST);
 		GLES20.glClearDepthf(1.0f);
@@ -92,43 +95,31 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
 	}
 	
 	private void initScene() {
-		planes = new Plane[6];
+		mLightPos[0] = -1f;
+		mLightPos[1] = -1f;
 		
-		planes[0] = new Plane(basicShader, //Top
-				new Vector3(-0.5f, 0.5f, -0.5f),
-				new Vector3(0, 0, 1),
-				new Vector3(1, 0, 0), 1);
-		planes[0].setColor(new float[] {1f,1f,0f,1}); // YELLOW
+		colors = new float[9][4];
+		colors[0] = new float[] {1f,1f,0f,1}; // YELLOW
+		colors[1] = new float[] {1f,0f,0f,1}; // RED
+		colors[2] = new float[] {0f,0f,1f,1}; // BLUE
+		colors[3] = new float[] {1f,0f,1f,1}; // MAGENTA
+		colors[4] = new float[] {0f,1f,0f,1}; // GREEN
+		colors[5] = new float[] {0f,1f,1f,1}; // CYAN
+		colors[6] = new float[] {0.1f,0.6f,0.6f,1};
+		colors[7] = new float[] {0.4f,0.1f,0.9f,1};
+		colors[8] = new float[] {1f,1f,1f,1}; // WHITE
 		
-		planes[1] = new Plane(basicShader,	//Left
-				new Vector3(-0.5f, -0.5f, -0.5f),
-				new Vector3(0, 1, 0),
-				new Vector3(0, 0, 1), 1);
-		planes[1].setColor(new float[] {1f,0f,0f,1}); // RED
+		planes = new Plane[9];
 		
-		planes[2] = new Plane(basicShader,	//Bot
-				new Vector3(-0.5f, -0.5f, 0.5f),
-				new Vector3(0, 0, -1),
-				new Vector3(1, 0, 0), 1);
-		planes[2].setColor(new float[] {0f,1f,0f,1}); // GREEN
-		
-		planes[3] = new Plane(basicShader,	//Right
-				new Vector3(0.5f, -0.5f, 0.5f),
-				new Vector3(0, 1, 0),
-				new Vector3(0, 0, -1), 1);
-		planes[3].setColor(new float[] {0f,0f,1f,1}); // BLUE
-		
-		planes[4] = new Plane(basicShader,	//Front
-				new Vector3(-0.5f, -0.5f, 0.5f),
-				new Vector3(0, 1, 0),
-				new Vector3(1, 0, 0), 1);
-		planes[4].setColor(new float[] {1f,0f,1f,1}); // MAGENTA
-		
-		planes[5] = new Plane(basicShader,	//Back
-				new Vector3(0.5f, -0.5f, -0.5f),
-				new Vector3(0, 1, 0),
-				new Vector3(-1, 0, 0), 1);
-		planes[5].setColor(new float[] {0f,1f,1f,1}); // CYAN
+		for (int i = 0; i < 3; ++i) {
+			for (int j = 0; j < 3; ++j) {
+				planes[i*3+j] = new Plane(basicShader,	//Front
+						new Vector3(-1.5f+1f*i, -1.5f+1f*j, 0f),
+						new Vector3(0, 1, 0),
+						new Vector3(1, 0, 0), 1);
+				planes[i*3+j].setColor(colors[i*3+j]); // MAGENTA
+			}
+		}
 	}
 
 	public void onDrawFrame(GL10 unused) {
@@ -165,6 +156,10 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
         GameGLRenderer.checkGlError("glGetUniformLocation");
         GLES20.glUniformMatrix4fv(mNormalHandle, 1, false, mNormalMatrix, 0);
         GameGLRenderer.checkGlError("glUniformMatrix4fv");
+        
+		int mLlightPosPointer = GLES20.glGetUniformLocation(basicShader, "lightPos");
+        GameGLRenderer.checkGlError("glGetUniformLocation");
+		GLES20.glUniform2fv(mLlightPosPointer, 1, mLightPos, 0);
 		
 		for (int i = 0; i < planes.length; ++i) {
 			planes[i].draw(mMVPMatrix);
@@ -234,8 +229,41 @@ public class GameGLRenderer implements GLSurfaceView.Renderer {
 				}
 				break;
 			case Stopping:
-				mVXAngle -= mVXAngle*2*deltaTime;
-				mVYAngle -= mVYAngle*2*deltaTime;
+				Matrix.setIdentityM(mCurrRotation, 0);
+				mXAngle = mYAngle = 0f;
+				mVXAngle = mVYAngle = 0f;
+		}
+		
+		float lightSpeed = 1.0f;
+		switch (mLightState) {
+			case 0:
+				mLightPos[0] += deltaTime*lightSpeed;
+				if (mLightPos[0] > 1.0f) {
+					mLightPos[0] = 1.0f;
+					mLightState = 1;
+				}
+				break;
+			case 1:
+				mLightPos[1] += deltaTime*lightSpeed;
+				if (mLightPos[1] > 1.0f) {
+					mLightPos[1] = 1.0f;
+					mLightState = 2;
+				}
+				break;
+			case 2:
+				mLightPos[0] -= deltaTime*lightSpeed;
+				if (mLightPos[0] < -1.0f) {
+					mLightPos[0] = -1.0f;
+					mLightState = 3;
+				}
+				break;
+			case 3:
+				mLightPos[1] -= deltaTime*lightSpeed;
+				if (mLightPos[1] < -1.0f) {
+					mLightPos[1] = -1.0f;
+					mLightState = 0;
+				}
+				break;
 		}
 	}
 	
